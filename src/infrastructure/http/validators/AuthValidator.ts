d48b4ba53ddfdff56cc2467d3ecc3efe61e892ad
joao <joao.simsic@4x4brasil.com.br>
2025-12-26 20:08:32 -0300

    fix: auth validator message

--- src/infrastructure/http/validators/AuthValidator.ts
+++ src/infrastructure/http/validators/AuthValidator.ts
@@ -25,7 +25,7 @@ export const authSchema = z.object({
 				}),
 			{
 				message:
-					"Password must contain at least one uppercase letter and one number",
+					"Password must contain at least one uppercase and lowercase letter and one number",
 			},
 		),
 });
